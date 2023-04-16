d8d07f0e90c6b32144f9fbe7d81cec6dfc7d3493
Craig Wright <cwrigh215@caledonian.ac.uk>
2023-04-16 23:31:59 +0100

    apk file i hope

---
+++ app/build/generated/source/buildConfig/androidTest/debug/org/me/gcu/wright_craig_s2023653/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.me.gcu.wright_craig_s2023653.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.me.gcu.wright_craig_s2023653.test";
+  public static final String BUILD_TYPE = "debug";
+}
